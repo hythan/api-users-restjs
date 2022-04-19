28a70c5fcf6c847e5bb7d82752730ef767ff447f
Hythan <hythan97@gmail.com>
2022-04-18 23:42:00 -0300

    Adicionado retorno de user ao logar

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
     private usersService: UsersService,
     private jwtService: JwtService,
     private bcrypt: BcryptService,
-  ) {}
+  ) { }
 
   async validateUser(userEmail: string, userPassword: string) {
     const user = await this.usersService.findByEmail(userEmail);
@@ -23,7 +23,13 @@ export class AuthService {
   async login(user: any) {
     const payload = { email: user.email, sub: user.id };
     return {
-      access_token: this.jwtService.sign(payload),
+      token: this.jwtService.sign(payload),
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
     };
   }
 }
